51a75a46d61c6b80636d75efb9e9221662011611
VendelK <panleo97@hotmail.com>
2025-03-03 15:12:55 +0100

    Made a simple function test for the moisture sensor. It will work with a little bit of drift, but should be manageable.

---
+++ Arduino/Pico-FuktsensorTest/Pico-FuktsensorTest.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <SD.h>
+
+File logFile;
+
+//Pin definitions
+int SENSOR_PIN[] = { A0, A1 };
+
+int sensorReading[2];
+unsigned long myIndex = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  {};
+
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SS)) {
+    Serial.println("Failed to initialize SD card");
+    while (1)
+      ;
+  } else {
+    Serial.println("Successfully initialized card");
+  }
+
+  logFile = SD.open("fuktlog.txt", FILE_WRITE);
+  logFile.println("index,sensor1,sensor2");
+  logFile.close();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 2; i++) {
+    sensorReading[i] = analogRead(SENSOR_PIN[i]);
+  }
+
+  logFile = SD.open("fuktlog.txt", FILE_WRITE);
+  logFile.println(
+    String(myIndex)+","+
+    String(sensorReading[0])+","+
+    String(sensorReading[1])
+    );
+  
+  logFile.close();
+  myIndex++;
+  delay(1800000); //wait for half an hour.
+}
