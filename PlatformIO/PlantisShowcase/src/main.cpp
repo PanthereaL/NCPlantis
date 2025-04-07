2f4af0dfc9be118192ae42614487abadfeb5b87d
VendelK <panleo97@hotmail.com>
2025-04-07 12:45:06 +0200

    Did a blink test to confirm PlatformIO could connect and flash to RP Pico

--- PlatformIO/PlantisShowcase/src/main.cpp
+++ PlatformIO/PlantisShowcase/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
+int LED_PIN = 0;
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
