516b0bfa12dcc881581f95a84bfe28693b3c3cd9
VendelK <panleo97@hotmail.com>
2025-04-08 17:46:32 +0200

    Set up platformio.ini and set up the organizer folders to work on tests and the showcase mdoel

---
+++ PlatformIO/PlantisShowcase/src/massTest/main.cpp
@@ -0,0 +1,9 @@
+/* 
+ * This is supposed to be the simple code for the long term moisture tests
+ * for the project. It will have two MCUs reading from five sensors, same as 
+ * the simple tests from before.
+ */
+
+#include <Arduino.h>
+#include "SPI.h"
+#include "SD.h"
--- PlatformIO/PlantisShowcase/src/sensor/main.cpp
+++ PlatformIO/PlantisShowcase/src/sensor/main.cpp
@@ -1,19 +1,16 @@
 #include <Arduino.h>
 
-int LED_PIN = 0;
+int MOISTURE_SENSOR_PIN = A0;
+int LIGHT_SENSOR_PIN1 = A1;
+int
 // put function declarations here:
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
 }
 
 // put function definitions here:
