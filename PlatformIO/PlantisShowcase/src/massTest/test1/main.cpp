928a285ab00b6476241ba0d958fcba84ac6f56b1
VendelK <panleo97@hotmail.com>
2025-04-10 17:19:14 +0200

    Built out the mass test with support for multiplexing, allowing for up to eight sensors on a single board. All it's missing is to implement the beepers, and it's fully functional, though it could be expanded with libraries and classes.

--- PlatformIO/PlantisShowcase/src/massTest/test1/main.cpp
+++ PlatformIO/PlantisShowcase/src/massTest/test1/main.cpp
@@ -12,9 +12,14 @@
 
 //Variable declarations
 File logFile;
+const int numOfSensors = 5;
+int sensorRead[numOfSensors];
+unsigned long nextRead;
+int currentIndex = 0;
 
 //Pin definition
-int SENSOR_PIN[] = {};
+int SENSOR_PIN[] = {A0, A1};
+int SELECT_PIN[] = {1, 2};
 int BUZZER_PIN = 3;
 
 void setup() {
@@ -24,6 +29,8 @@ void setup() {
 
     //Deal with pin modes
     pinMode(BUZZER_PIN,OUTPUT);
+    pinMode(SELECT_PIN[0],OUTPUT);
+    pinMode(SELECT_PIN[1],OUTPUT);
 
     if(!Serial) {
         delay(1500); //Wait a second and a half just to make sure Serial is ready.
@@ -42,17 +49,36 @@ void setup() {
 
     //setup SD card with header
     logFile = SD.open("fuktlog.txt", FILE_WRITE);
-    logFile.println("index,Sensor1,Sensor2,Sensor3");
+    logFile.println("index,Sensor1,Sensor2,Sensor3,Sensor4,Sensor5");
     logFile.close();
 
     //play startup beep TODO
 
     while (millis() < 5000) {} //Just waits until the system has been on for 5 seconds
+    nextRead = millis() + 300000; //Now + 5 min.
 }
 
 void loop() {
+    currentIndex++;
     //read from sensors
+    for (int i = 0 ; i < numOfSensors ; i++) {
+        digitalWrite(SELECT_PIN[0], (i & 0b100) >> 2);
+        digitalWrite(SELECT_PIN[1], (i & 0b010) >> 1);
+        sensorRead[i] = analogRead(SENSOR_PIN[i%2]);
+    }
     //write to SD card
-    //play action complete beep
+    String tmp_output = String(currentIndex);
+    for (int i = 0 ; i < numOfSensors ; i++) {
+        tmp_output += ("," + sensorRead[i]);
+    }
+
+    logFile = SD.open("fuktlog.txt", FILE_WRITE);
+    logFile.println(tmp_output);
+    logFile.close();
+
+    //play action complete beep TODO
+
     //wait until next time
+    while (millis() < nextRead);
+    nextRead = nextRead + 300000;
 }
