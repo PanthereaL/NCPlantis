0f36f1a47c01be16085f06fc00ef6cb1f25172ad
VendelK <panleo97@hotmail.com>
2025-04-28 16:57:08 +0200

    Added library for RF tranceiver and wrote test code for one test.

--- PlatformIO/PlantisShowcase/src/sensor/main.cpp
+++ PlatformIO/PlantisShowcase/src/sensor/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
+#include "SPI.h"
+#include "RF24.h"
+
+RF24 radio (7, 8);
 
 int MOISTURE_SENSOR_PIN = A0;
 int LIGHT_SENSOR_PIN1 = A1;
-int
+const byte address[5] = {'0', '0', '0', '0', '1'};
+const byte test[6] = "00002";
 // put function declarations here:
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  const char text[] = "Hello RF!";
+  bool success = radio.write(&text, sizeof(text));
+  if (success) {
+    Serial.println("Sent successfully");
+  } else {
+    Serial.println("Failed to send.");
+  }
 }
 
 // put function definitions here:
