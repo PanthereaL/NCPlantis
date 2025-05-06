28fe92d63761df34f6a7bfc037d1c618036be1b2
VendelK <panleo97@hotmail.com>
2025-05-06 13:10:29 +0200

    Put the hub code throug Arduino

---
+++ Arduino/hubArduino/hubArduino.ino
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include "SPI.h"
+#include "RF24.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+
+#define TFT_CS 16
+#define TFT_RST 23
+#define TFT_DC 22
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+RF24 radio (0, 17); //CE, CNS
+
+const byte address[5] = {'0', '0', '0', '0', '1'};
+// put function declarations here:
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+
+  Serial.println("Serial opened");
+  bool connected = radio.begin(); 
+  if (connected) {
+    Serial.println("Successfully initialized radio tranceiver.");
+  } else {
+    Serial.println("Failed to initialize radio tranceiver.");
+  }
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening();
+
+  tft.init(240, 320, SPI_MODE2);
+  tft.invertDisplay(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(20, 40);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Hello, ESP!");
+}
+
+void loop() {
+  
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
+
+// put function definitions here:
