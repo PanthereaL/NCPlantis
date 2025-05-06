94da938de60c56c667a2fa7b9b9939ce4d0e72b2
VendelK <panleo97@hotmail.com>
2025-05-06 19:16:55 +0200

    Hub code fiddled with to learn more about screen

--- Arduino/hubArduino/hubArduino.ino
+++ Arduino/hubArduino/hubArduino.ino
@@ -13,7 +13,19 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 RF24 radio (0, 17); //CE, CNS
 
 const byte address[5] = {'0', '0', '0', '0', '1'};
+
+uint16_t lineSpace = 20; //Pixels between each line with text size 2.
+
 // put function declarations here:
+void printToScreen(
+  const char *text, 
+  uint16_t x = 20, 
+  uint16_t y = 40, 
+  uint16_t textSize = 2, 
+  uint16_t textColor = ST77XX_BLACK
+);
+
+void screenClear(uint16_t color = ST77XX_WHITE);
 
 void setup() {
   Serial.begin(9600);
@@ -32,11 +44,7 @@ void setup() {
 
   tft.init(240, 320, SPI_MODE2);
   tft.invertDisplay(false);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(20, 40);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(2);
-  tft.print("Hello, ESP!");
+  printToScreen("Hello ESP!");
 }
 
 void loop() {
@@ -45,7 +53,32 @@ void loop() {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
+    printToScreen(text);
   }
+  screenClear();
+  printToScreen("OVERSKRIFT",10,10,3,ST77XX_RED);
+  printToScreen("jgyjgyTEST");
+  printToScreen("llllllDEBUG",20,60);
+  delay(1000);
+  
 }
 
 // put function definitions here:
+
+void printToScreen(
+  const char *text, 
+  uint16_t x, 
+  uint16_t y, 
+  uint16_t textSize, 
+  uint16_t textColor
+)
+{
+  tft.setCursor(x,y);
+  tft.setTextColor(textColor);
+  tft.setTextSize(textSize);
+  tft.print(text);
+}
+
+void screenClear(uint16_t color) {
+  tft.fillScreen(color);
+}
